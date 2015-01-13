0393d2185a063e5367c54e26f3478027a6d30a50
J-F B <dontbelievethebyte@gmail.com>
2015-01-13 16:37:20 -0500

    Shuffled files around to make more sense. Fixed a few includes paths.

--- cms/web/index.php
+++ cms/web/index.php
@@ -7,15 +7,15 @@
  * @copyright (c) 2013, J-F B
  * @license http://www.gnu.org/licenses/gpl-3.0.html
  */
-require_once './includes/lib/processor.inc';
-require_once './includes/lib/frontController.inc';
-require_once './includes/lib/request.inc';
-require_once './includes/lib/commandMapper.inc';
-require_once './includes/lib/viewController.inc';
-require_once './includes/lib/rewrite.inc';
-require_once './includes/lib/siteConfig.inc';
-require_once './includes/lib/db.inc';
-require_once './includes/lib/logger.inc';
+require_once '../includes/lib/processor.inc';
+require_once '../includes/lib/frontController.inc';
+require_once '../includes/lib/request.inc';
+require_once '../includes/lib/commandMapper.inc';
+require_once '../includes/lib/viewController.inc';
+require_once '../includes/lib/rewrite.inc';
+require_once '../includes/lib/siteConfig.inc';
+require_once '../includes/lib/db.inc';
+require_once '../includes/lib/logger.inc';
 try {
     $controller = new SprdCMSFrontController(
         new SprdCMSRequest(),
